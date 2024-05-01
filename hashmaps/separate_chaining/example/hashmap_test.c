8768b3b6e2a537efa1b00979960b8bc8d999779f
Jacob <jacob@notactivelylooking.com>
2024-05-01 17:59:37 +0100

    simple free method.

--- hashmaps/separate_chaining/example/hashmap_test.c
+++ hashmaps/separate_chaining/example/hashmap_test.c
@@ -5,5 +5,6 @@
 int main(void) {
     hashmap *map = newHashmap(100);
 
+    freeHashmap(map);
     return 0;
 }
--- hashmaps/separate_chaining/src/hashmap.c
+++ hashmaps/separate_chaining/src/hashmap.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
+#include "bucket.h"
 #include "hashmap.h"
 
 hashmap *newHashmap(size_t capacity) {
@@ -7,4 +8,31 @@ hashmap *newHashmap(size_t capacity) {
     map->capacity = capacity;
     map->items = calloc(map->capacity, sizeof(bucket*));
     return map;
+}
+
+void hashmapAdd(hashmap *map, bucket *item) {
+
+}
+
+void hashmapSearch(hashmap *map, const char *key) {
+
+}
+
+bucket *hashmapGet(hashmap *map, const char *key) {
+
+}
+
+void printHashmap(hashmap *map) {
+
+}
+
+void freeHashmap(hashmap *map) {
+    for (int i = 0; i < map->capacity; i++) {
+        bucket *item = map->items[i];
+        if (item != NULL) {
+            freeBucket(item);
+        }
+    }
+    free(map->items);
+    free(map);
 }
