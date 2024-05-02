1af1952bf94d6ad31aaa7d4165a40aaf6b58f20f
Jacob <jacob@notactivelylooking.com>
2024-05-02 23:57:11 +0100

    optimise tests.

--- hashmaps/separate_chaining/example/hashmap_test.c
+++ hashmaps/separate_chaining/example/hashmap_test.c
@@ -1,62 +1,72 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "bucket.h"
 #include "hashmap.h"
 
-const size_t MAP_SIZE = 10;
 const size_t ARRAY_SIZE = 10;
-const size_t SEARCH_ARRAY_SIZE = 11;
+
+static const char alphabet[27] = {
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+    'O','P','Q','R','S','T','U','V','W','X','Y','Z','\0' 
+};
+
+char *generateRandomString(size_t length) {
+    char *str = calloc(length+1, sizeof(char));
+    for (size_t i = 0; i < length; i++) {
+        str[i] = alphabet[rand() % 26];
+    }
+    str[length] = '\0';
+    return str;
+}
+
+char **generateKeys(size_t numberOfKeys, size_t stringLength) {
+    char **keys = calloc(numberOfKeys, sizeof(char*));
+    for (int i = 0; i < numberOfKeys; i++) {
+        keys[i] = generateRandomString(stringLength);
+    }
+    return keys;
+}
+
+void testExists(hashmap *map, char **searchKeys, size_t arraySize) {
+    for (int i = 0; i < arraySize; i++) {
+        const char *search = searchKeys[i];
+        printf("Key '%s' exists in map: %d\n", search, hashmapExists(map, search));
+    }
+}
+
+void testGets(hashmap *map, char **searchKeys, size_t arraySize) {
+    for (int i = 0; i < arraySize; i++) {
+        const char *search = searchKeys[i];
+        bucket *item = hashmapGet(map, search);
+        if (item != NULL) {
+            printf("Key '%s': ", search);
+            printBucket(item);
+        } else {
+            printf("Key '%s': NONE\n", search);
+        }
+    }
+}
 
 void basicTest(void) {
-    hashmap *map = newHashmap(MAP_SIZE);
+    hashmap *map = newHashmap(1000);
 
-    char keys[ARRAY_SIZE][10] = {
-        "cat",
-        "dog",
-        "bat",
-        "elephant",
-        "rat",
-        "lion",
-        "wolf",
-        "giraffe",
-        "bobcat",
-        "tiger",
-    };
+    char **keys = generateKeys(ARRAY_SIZE, 10);
 
     for (int i = 0; i < ARRAY_SIZE; i++) {
         bucket *item = newBucket(keys[i], "animal");
-        printBucket(item);
+        // printBucket(item);
         hashmapAdd(map, item);
     }
     printHashmap(map);
 
-    char searchKeys[SEARCH_ARRAY_SIZE][10] = {
-        "cat",
-        "dog",
-        "bat",
-        "elephant",
-        "rat",
-        "lion",
-        "wolf",
-        "giraffe",
-        "bobcat",
-        "tiger",
-        "one"
-    };
+    char **searchKeys = generateKeys(ARRAY_SIZE, 10);
 
-    for (int i = 0; i < SEARCH_ARRAY_SIZE; i++) {
-        const char *search = searchKeys[i];
-        printf("Key '%s' exists in map: %d\n", search, hashmapExists(map, search));
-    }
+    testExists(map, keys, ARRAY_SIZE);
+    testExists(map, searchKeys, ARRAY_SIZE);
 
-    for (int i = 0; i < SEARCH_ARRAY_SIZE; i++) {
-        const char *search = searchKeys[i];
-        bucket *item = hashmapGet(map, search);
-        if (item != NULL) {
-            printf("Key '%s' exists!!! ", search);
-            printBucket(item);
-        }
-    }
+    testGets(map, keys, ARRAY_SIZE);
+    testGets(map, searchKeys, ARRAY_SIZE);
 
     freeHashmap(map);
 }
