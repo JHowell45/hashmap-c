07f4170498fb8045addd034189e4fbe6939ad4e6
Jacob <jacob@notactivelylooking.com>
2024-05-02 23:32:28 +0100

    new definition for resizable map and methods.

--- hashmaps/separate_chaining/include/hashmap.h
+++ hashmaps/separate_chaining/include/hashmap.h
@@ -25,4 +25,22 @@ void printHashmap(hashmap *map);
 
 void freeHashmap(hashmap *map);
 
+typedef struct {
+    size_t capacity;
+    size_t count;
+    bucket **items;
+} resize_hashmap;
+
+hashmap *newResizeHashmap(size_t capacity);
+
+void resizeHashmapAdd(resize_hashmap *map, bucket *item);
+
+bool hashmapExists(hashmap *map, const char *key);
+
+bucket *hashmapGet(hashmap *map, const char *key);
+
+void printResizeHashmap(resize_hashmap *map);
+
+void freeResizehashmap(resize_hashmap *map);
+
 #endif
